c5c3a116f49fce22e3de034226b8952317667629
Kayque010 <10425657@mackenzista.com.br>
2025-03-19 23:33:16 +0000

    Commit de alteração

--- Lab04/src/App.java
+++ Lab04/src/App.java
@@ -65,11 +65,10 @@ public class App {
     }
 
     public static void main(String[] args) throws Exception {
-
         create();
         read();
         update();
         delete();
-        }
+      }
     }
 
